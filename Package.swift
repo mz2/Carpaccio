7a67bf0c114fce843eac0f9b62bae5a861c9b87a
Matias Piipari <matias.piipari@gmail.com>
2021-09-04 23:16:51 +0300

    Trying to up the version requirement

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "Carpaccio",
-    platforms: [.macOS(.v10_14),
-                .iOS(.v13)],
+    platforms: [.macOS(.v10_15),
+                .iOS(.v14)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
