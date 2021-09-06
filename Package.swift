b18cb5e374eda030b637320b70e9b2fa92104f8a
Matias Piipari <matias.piipari@gmail.com>
2021-09-06 08:41:05 +0300

    Declare Carpaccio as a dynamic framework

--- Package.swift
+++ Package.swift
@@ -8,9 +8,9 @@ let package = Package(
     platforms: [.macOS(.v10_15),
                 .iOS(.v14)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Carpaccio",
+            type: .dynamic,
             targets: ["Carpaccio"]
         ),
         .executable(name: "exifdump", targets: ["exifdump"])
