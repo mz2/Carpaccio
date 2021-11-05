3ed871e05892f15187d30487a1593f56a173c691
Markus Piipari <markus@cosmicjetpack.com>
2021-11-05 09:19:40 +0200

    Resolve the CIRAWFilterOption.ciInputEnableEDRModeKey vs. kCIInputEnableEDRModeKey API change issue

--- Package.swift
+++ Package.swift
@@ -1,12 +1,14 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Carpaccio",
-    platforms: [.macOS(.v10_15),
-                .iOS(.v14)],
+    platforms: [
+        .macOS(.v11),
+        .iOS(.v14)
+    ],
     products: [
         .library(
             name: "Carpaccio",
